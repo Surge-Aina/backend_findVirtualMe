74198fd480f09b1145e650cb0661767c6c5d8df6
cgarcia1196 <cgarcia1196@gmail.com>
2025-08-12 11:14:07 -0700

    added files from project manager porftolio project

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoute');
+const portfolioRoutes = require('./routes/portfolioRoute');
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-//app.use('/portfolio', portfolioRoute);
 //jaqueline login route
 app.use('/user', userRoutes);
+app.use('/portfolio', portfolioRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).json({
